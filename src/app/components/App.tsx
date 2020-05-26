bf4f903108df9acff450dc2b7764de4ebc558085
johnthedesigner <john@johnthedesigner.com>
2020-05-26 13:07:13 -0400

    Fix some ts errors and warnings

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,80 +1,86 @@
-import * as React from 'react';
-import '../styles/ui.css';
-import generateColors from '../../../utils/generateColors.js';
+import * as React from "react";
+import _ from "lodash";
 
-declare function require(path: string): any;
+import "../styles/ui.css";
+import generateColors from "../../../utils/generateColors.js";
+
+// declare function require(path: string): any;
+
+type AppState = {
+  seedColors: string[];
+};
 
 class App extends React.Component<{}, AppState> {
-    constructor(props) {
-        super(props);
-        this.state = {
-            seedColors: ['red', 'blue'],
-        };
-    }
-
-    addSeed = () => {
-        let newSeedColors = [...this.state.seedColors];
-        newSeedColors[this.state.seedColors.length] = '';
-        this.setState({seedColors: newSeedColors});
+  constructor(props) {
+    super(props);
+    this.state = {
+      seedColors: ["red", "blue"]
     };
+  }
 
-    buildGradients = () => {
-        return _.map(this.state.seedColors, seedColor => {
-            return generateColors(seedColor);
-        });
-    };
+  addSeed = () => {
+    let newSeedColors = [...this.state.seedColors];
+    newSeedColors[this.state.seedColors.length] = "";
+    this.setState({ seedColors: newSeedColors });
+  };
 
-    onChange = (e, i) => {
-        let newSeedColors = [...this.state.seedColors];
-        newSeedColors[i] = e.target.value;
-        this.setState({seedColors: newSeedColors});
-    };
+  buildGradients = () => {
+    return _.map(this.state.seedColors, seedColor => {
+      return generateColors(seedColor);
+    });
+  };
 
-    onCreate = () => {
-        parent.postMessage(
-            {
-                pluginMessage: {
-                    type: 'create-palette',
-                    palettes: this.buildGradients(),
-                },
-            },
-            '*'
-        );
-    };
+  onChange = (e, i) => {
+    let newSeedColors = [...this.state.seedColors];
+    newSeedColors[i] = e.target.value;
+    this.setState({ seedColors: newSeedColors });
+  };
 
-    onCancel = () => {
-        parent.postMessage({pluginMessage: {type: 'cancel'}}, '*');
-    };
+  onCreate = () => {
+    parent.postMessage(
+      {
+        pluginMessage: {
+          type: "create-palette",
+          palettes: this.buildGradients()
+        }
+      },
+      "*"
+    );
+  };
 
-    render() {
-        return (
-            <div>
-                <h2>Palette Creator</h2>
-                {_.map(this.state.seedColors, (seedColor, i) => {
-                    return (
-                        <p key={i}>
-                            Color:{' '}
-                            <input
-                                type="text"
-                                name={`color`}
-                                value={this.state.seedColors[i]}
-                                required
-                                onChange={e => this.onChange(e, i)}
-                            />
-                        </p>
-                    );
-                })}
-                <button id="addColor" onClick={this.addSeed}>
-                    Add Color
-                </button>
-                <hr />
-                <button id="create" onClick={this.onCreate}>
-                    Create Palette
-                </button>
-                <button onClick={this.onCancel}>Cancel</button>
-            </div>
-        );
-    }
+  onCancel = () => {
+    parent.postMessage({ pluginMessage: { type: "cancel" } }, "*");
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Palette Creator</h2>
+        {_.map(this.state.seedColors, (seedColor, i) => {
+          return (
+            <p key={i}>
+              Color:{" "}
+              <input
+                type="text"
+                name={`color`}
+                value={seedColor}
+                required
+                onChange={e => this.onChange(e, i)}
+              />
+            </p>
+          );
+        })}
+        <button id="addColor" onClick={this.addSeed}>
+          Add Color
+        </button>
+        <hr />
+        <button id="create" onClick={this.onCreate}>
+          Create Palette
+        </button>
+        <button onClick={this.onCancel}>Cancel</button>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/plugin/controller.ts
+++ src/plugin/controller.ts
@@ -6,12 +6,12 @@ async function createRectangles(palettes) {
   const allSwatches = new Array();
   const nodes = new Array();
 
-  const whitePaint = {
+  const whitePaint: SolidPaint = {
     type: "SOLID",
     color: { r: 1, g: 1, b: 1 }
   };
 
-  const blackPaint = {
+  const blackPaint: SolidPaint = {
     type: "SOLID",
     color: { r: 0, g: 0, b: 0 }
   };
@@ -21,7 +21,7 @@ async function createRectangles(palettes) {
     let r = swatch.rgb[0] / 255;
     let g = swatch.rgb[1] / 255;
     let b = swatch.rgb[2] / 255;
-    let paint = {
+    let paint: SolidPaint = {
       type: "SOLID",
       color: { r, g, b }
     };
@@ -115,43 +115,8 @@ async function createRectangles(palettes) {
   };
   gradientLoop(0);
 
-  // // Reveal swatches one at a time
-  // async function fadeLoop(currentNode, fadeIndex, nodeIndex) {
-  //   let movementPerStep = 0.1;
-  //   let distance = 1 - currentNode.opacity;
-  //   let movement = distance * movementPerStep;
-  //   if (currentNode.opacity < 0.95) {
-  //     let initialDelay = fadeIndex === 0 ? 50 * nodeIndex : 20;
-  //     await setTimeout(() => {
-  //       currentNode.opacity = currentNode.opacity + movement;
-  //       fadeLoop(currentNode, fadeIndex + 1, nodeIndex);
-  //     }, initialDelay);
-  //   } else {
-  //     currentNode.opacity = 1;
-  //   }
-  // }
-
-  // async function showSwatches() {
-  //   for (var i = 0; i < nodes.length; i++) {
-  //     await fadeLoop(nodes[i], 0, i);
-  //   }
-  // }
-  // await showSwatches();
   figma.currentPage.selection = nodes;
   figma.closePlugin();
-
-  // const revealLoop = (swatchIndex) => {
-  //   if (swatchIndex < nodes.length) {
-  //     let currentNode = nodes[swatchIndex];
-  //     fadeLoop(currentNode, 0);
-  //
-  //     revealLoop(swatchIndex + 1);
-  //   } else {
-  //     figma.currentPage.selection = nodes;
-  //     figma.closePlugin();
-  //   }
-  // }
-  // revealLoop(0);
 }
 
 figma.showUI(__html__, { width: 300, height: 300 });
